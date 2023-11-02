c1e2bcfabef4ec683e0fa879908f9c4320a16e78
Halfbloodprince3109 <abdxtreme07@gmail.com>
2023-11-02 16:40:01 +0500

    restored image display version of splash screen and commented new tested ideas for animated display to work on other screens

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,12 +1,9 @@
 import 'dart:async';
-import 'dart:math';
-//import 'package:get/get.dart';
-
+// import 'dart:math';
 import 'package:get/get.dart';
-import 'package:namaz_mode_app/Hexagon_Painter_splashscreen.dart';
+// import 'package:namaz_mode_app/Hexagon_Painter_splashscreen.dart';
 import 'temporary_home.dart';
 import 'package:flutter/material.dart';
-
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -14,7 +11,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  int duration = 50;
+  int duration = 5;
 
   @override
   void initState() {
@@ -32,24 +29,60 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Container(
-      color: Color(0xFF012323),
-      width: Get.width,
-      height: Get.height,
-      child: Transform.rotate(
-        angle: pi / 2,
-        child: CustomPaint(
-          size: MediaQuery.of(context).size,
-          painter: HexagonPainter(),
-          child: Center(
-            child: Text(
-              "Content goes here",
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Image.asset(
+          'assets/ssnn.png',
+          fit: BoxFit.fitWidth,
         ),
       ),
-    )));
+    );
   }
 }
+
+// class SplashScreen extends StatefulWidget {
+//   @override
+//   _SplashScreenState createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   int duration = 50;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     Timer(
+//       Duration(seconds: duration),
+//       () {
+//         Navigator.of(context).pushReplacement(
+//           MaterialPageRoute(builder: (context) => HomeScreen()),
+//         );
+//       },
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Center(
+//             child: Container(
+//       color: Color(0xFF012323),
+//       width: Get.width,
+//       height: Get.height,
+//       child: Transform.rotate(
+//         angle: pi / 2,
+//         child: CustomPaint(
+//           size: MediaQuery.of(context).size,
+//           painter: DiamondHexagonPainter(),
+//           child: Center(
+//             child: Text(
+//               "Content goes here",
+//               style: TextStyle(color: Colors.white),
+//             ),
+//           ),
+//         ),
+//       ),
+//     )));
+//   }
+// }
